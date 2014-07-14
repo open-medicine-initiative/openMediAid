eafde855aa9022df554f2beb1c9745891df8f4c6
Benjamin Diedrichsen <b.diedrichsen@gmail.com>
2014-07-14 15:29:00 +0200

    added code template for creating a unit test

---
+++ code/src/code-templates/test.js
@@ -0,0 +1,20 @@
+var expect = require("expect.js")
+
+describe('A testcase:', function(){
+
+
+    beforeEach(function (done){
+        done();
+    });
+    afterEach(function (done){
+        done();
+    });
+
+    describe('a sub group', function(){
+        it('a test', function(){
+            // write test here
+        });
+    })
+
+})
+
