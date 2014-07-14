45e8bb291311dff061566de619b27ed1f29690ac
Benjamin Diedrichsen <b.diedrichsen@gmail.com>
2014-07-14 15:29:28 +0200

    extended trait skeleton

--- code/src/code-templates/TTrait.js
+++ code/src/code-templates/TTrait.js
@@ -2,14 +2,16 @@ var DSM = require('./dsm');
 var Trait = DSM.TraitJS;
 
 function TName(){
+    // PRIVATE
 
 
+    // PUBLIC
     var Name = Trait({
 
 
 
     })
-    Name.initialize = function(){
+    Name.initialize = function(params){
         // empty initializer
     };
     return DSM.traitify(Name);
