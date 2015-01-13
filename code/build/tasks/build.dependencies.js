b734253060c256f0284fdd578e048223ef7b8a61
bennidi <b.diedrichsen@gmail.com>
2015-01-13 19:41:38 +0100

    fixed gulp bower task

--- code/build/tasks/build.dependencies.js
+++ code/build/tasks/build.dependencies.js
@@ -5,16 +5,17 @@ var gulp = require( 'gulp' ),
     browserify = require( 'browserify' ),
     Finder = require( "fs-finder" ),
     source = require("vinyl-source-stream" ),
-    bower = require('bower');
+    bower = require('bower' ),
+    gutil = require('gulp-util');
 
-gulp.task( 'dep:prepare', function ( callback ) {
+gulp.task( 'dep:prepare', function ( done ) {
     mkdirp( paths.reports, function ( err ) {
-        callback();
+        done();
     } );
 } );
 
 /**
- * rebundle node module cocktail to use in requirejs environment
+ * rebundle node module cocktail for usage in requirejs environment
  */
 gulp.task( 'dep:cocktail', function ( ) {
     return browserify({standalone:"cocktail"})
@@ -25,28 +26,15 @@ gulp.task( 'dep:cocktail', function ( ) {
         .pipe( gulp.dest( "./src/lib/cocktails" ) );
 } );
 
-gulp.task( 'dep:mochacakes', function ( ) {
-    return browserify({standalone:"mocha-cakes"})
-        .add("./node_modules/mocha-cakes/cakes.js")
-        .bundle()
-        // transform the stream to be gulp compatible
-        .pipe( source( 'cakes-bundle.js' ) )
-        .pipe( gulp.dest( "./node_modules/mocha-cakes" ) );
-} );
-
-gulp.task( 'dep', ['dep:cocktail'], function ( done ) {
+gulp.task( 'dep', ['dep:cocktail', 'dep:bower'], function ( done ) {
     done();
 } );
 
 // BOWER
-gulp.task('bower:install', function (callback) {
+gulp.task('dep:bower', function () {
     return bower.commands.install()
         .on('log', function (log) {
-            if (log.id !== 'install') { return; }
-            gutil.log('install', log.message);
-        })
-        .on('end', function (installed) {
-            callback();
+            gutil.log(log.id, log.message);
         });
 });
 
